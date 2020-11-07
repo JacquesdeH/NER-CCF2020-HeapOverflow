67484633b760855fdeab0dec37fd0cf8988062b9
JacquesdeH <jacquesdeh@buaa.edu.cn>
2020-11-07 23:50:33 +0800

    Version 1.2.0 | Add CCF dataset

--- core/config/DefaultConfig.py
+++ core/config/DefaultConfig.py
@@ -7,6 +7,9 @@ class DefaultConfig:
         IMAGE = os.path.join("image")
         LOG = os.path.join("log")
         CKPT = os.path.join("checkpoint")
+        DATA = os.path.join("data")
+        DATA_CCF = os.path.join(DATA, os.path.join("CCF"))
+        DATA_CCF_RAW = os.path.join(DATA_CCF, os.path.join("raw"))
 
     class HYPER:
         BATCH_SIZE = 32
