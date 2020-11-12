d40a5a1724b147ef19736533d7f24c7896141e9a
ubuntu <ubuntu@localhost.localdomain>
2020-11-12 09:16:11 +0800

    save

--- core/preprocessor/duplication_detector.py
+++ core/preprocessor/duplication_detector.py
@@ -18,10 +18,26 @@ class DuplicationDetector:
         raw_data_count = len(os.listdir(self.raw_data_set_dir + "/train/data"))
         self.logger.log_message("detect():", "raw data count:\t", raw_data_count)
         reader = LabelFileReader()
-        for i in range(raw_data_count):
-            data_file_name = self.raw_data_set_dir + "/train/data/" + str(i) + ".txt"
-            label_file_name = self.raw_data_set_dir + "/train/label/" + str(i) + ".csv"
-            
+        with open(self.duplication_info_dir + '/duplication.md', 'w', encoding='utf8') as dupfile:
+            dup_count = 0
+            for i in range(raw_data_count):
+                data_file_name = self.raw_data_set_dir + "/train/data/" + str(i) + ".txt"
+                label_file_name = self.raw_data_set_dir + "/train/label/" + str(i) + ".csv"
+                with open(label_file_name, 'r', encoding='utf8') as f:
+                    infos = reader.load(f)
+                infos.sort(key=lambda info: info.Pos_b)
+                for j in range(len(infos) - 1):
+                    if infos[j].Pos_e >= infos[j].Pos_b:
+                        self.logger.log_message("detect():\t", "detect duplication with id={:d}".format(infos[j].ID))
+                        dup_count += 1
+                        dupfile.write("---\n\n")
+                        dupfile.write("## {:d}\n\n".format(dup_count))
+                        dupfile.write("ID=[{:d}]\n\n".format(infos[j].ID))
+                        with open(data_file_name, 'r', encoding='utf8') as f:
+                            txt = f.read()
+                        dupfile.write(txt + '\n\n')
+                        dupfile.write("- [] [{:d}:{:d}]\t<{:s}>\t{:s}\n\n".format(infos[j].Pos_b, infos[j].Pos_e, infos[j].Category, infos[j].Privacy) )
+                        dupfile.write("- [] [{:d}:{:d}]\t<{:s}>\t{:s}\n\n".format(infos[j+1].Pos_b, infos[j+1].Pos_e, infos[j+1].Category, infos[j+1].Privacy))
 
 
         
--- core/preprocessor/label_file_reader.py
+++ core/preprocessor/label_file_reader.py
@@ -22,7 +22,7 @@ class LabelFileReader:
     def loads(self, line_content) -> LabelInfo:
         m = self.csv_re.match(line_content)
         if m == None:
-            self.logger.log_message("content cannot match pattern:", line_content)
+            self.logger.log_message("loads()\t:", "content cannot match pattern:", line_content)
             return None
         ID = int(m.group(1))
         Category = m.group(2)
@@ -39,16 +39,15 @@ class LabelFileReader:
         return ret
 
 
-    def load(self, file_name: str) -> "List[LabelInfo]":
+    def load(self, fp) -> "List[LabelInfo]":
         ret = []
-        with open(file_name, 'r', encoding='utf8') as f:
-            for row, line_content in enumerate(f.readlines()):
-                if row == 0:
-                    continue
-                new_info = self.loads(line_content)
-                if new_info is not None:
-                    ret.append(new_info)
-        self.logger.log_message(ret)
+        for row, line_content in enumerate(fp.readlines()):
+            if row == 0:
+                continue
+            new_info = self.loads(line_content)
+            if new_info is not None:
+                ret.append(new_info)
+        self.logger.log_message("load():\n", "infos:\t", ret)
         return ret
                 
     def dumps(self, info: LabelInfo):
@@ -62,7 +61,8 @@ class LabelFileReader:
 
 if __name__ == "__main__":
     reader = LabelFileReader()
-    infos = reader.load(DefaultConfig.PATHS.DATA_CCF_RAW + "/train/label/0.csv")
+    with open(DefaultConfig.PATHS.DATA_CCF_RAW + "/train/label/0.csv", 'r', encoding='utf8') as f:
+        infos = reader.load(f)
     print(reader.dumps(infos[0]))
     with open("label_file_reader.debug", 'w', encoding='utf8') as f:
        reader.dump(infos, f)
