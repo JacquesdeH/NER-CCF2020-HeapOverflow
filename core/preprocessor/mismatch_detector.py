50d747f2d45f37058fc444bf189b61b6f6608123
ubuntu <ubuntu@localhost.localdomain>
2020-11-16 18:01:00 +0800

    finish mismatch-detector

--- core/preprocessor/mismatch_detector.py
+++ core/preprocessor/mismatch_detector.py
@@ -1,7 +1,9 @@
 from ..utils import alloc_logger
+from .label_file_reader import LabelFileReader
 from .label_file_reader import LabelInfo
 from ..config.DefaultConfig import DefaultConfig
 import json
+import os
 from ..utils import alloc_logger
 
 
@@ -10,7 +12,7 @@ class MismatchDetector:
     def __init__(self, mismatch_file_dir: str=None):
         self.mismatch_count = 0
         self.fix_count = 0
-        self.alloc_logger = alloc_logger("mismatch_detector.log", MismatchDetector)
+        self.logger = alloc_logger("mismatch_detector.log", MismatchDetector)
         self.mismatch_file_dir = mismatch_file_dir if mismatch_file_dir is not None else DefaultConfig.PATHS.DATA_INFO
         
         
@@ -51,15 +53,36 @@ class MismatchDetector:
                         for info in infos:
                             if new_new_data[info.Pos_b : info.Pos_e + 1] != info.Privacy:
                                 tac["solved"] = "false"
+                                self.logger.log_message("mismatch not solve with id={:d}".format(infos[0].ID))
                                 return (None, None)
+                        self.logger.log_message("solve a mismatch with id={:d}".format(infos[0].ID))
                         return (new_new_data, new_infos)
                 else:
+                    self.logger.log_message("detect a mismatch with id={:d}".format(infos[0].ID))
                     msg = {
                         "solved": "false",
                         "data" : new_data,
                         "labels" : infos
                     }
-                    self.tactics[info.ID] = 
+                    self.tactics[info.ID] = msg
                     return (None, None)
         return (new_data, infos)
+
+
+if __name__ == "__main__":
+    reader = LabelFileReader()
+    detector = MismatchDetector()
+
+    data_dir = DefaultConfig.PATHS.DATA_CCF_RAW + '/data'
+    label_dir = DefaultConfig.PATHS.DATA_CCF_RAW + '/label'
+
+    data_count = len(os.listdir(data_dir))
+
+    for i in range(data_count):
+        with open(data_dir + "/{:d}.txt".format(i), 'r', encoding='utf8') as f:
+            data = f.read()
+        with open(label_dir + "/{:d}.csv".format(i), 'r', encoding='utf8') as f:
+            infos = reader.load(f)
+        detector.fix_mismatch(data, infos)
+
 
