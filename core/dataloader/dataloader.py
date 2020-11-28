99d0a1ed45fd496b40ba30d667b81d352fcc02cc
李明昕 <18376161@buaa.edu.cn>
2020-11-29 01:13:29 +0800

    temp instructor

--- core/dataloader/dataloader.py
+++ core/dataloader/dataloader.py
@@ -66,12 +66,12 @@ class CCFDataset(tud.Dataset):
         if self.in_train:
             with open(self.label + '/' + self.label_file_list[idx], encoding="utf8") as f_label:
                 label_list = json.load(f_label)
-                #label_list = [[(1 if count == sequence else 0) for count in range(self.label_dim)] for sequence in label_list]
+                # label_list = [[(1 if count == sequence else 0) for count in range(self.label_dim)] for sequence in label_list]
                 if len(label_list) >= self.seq_len:
                     label_list = label_list[:self.seq_len]
                 else:
                     label_list += [0 for count in range(self.seq_len - len(label_list))]
-                    #label_list += [[0] * DefaultConfig.HYPER.LABEL_DIM for count in range(self.seq_len - len(label_list))]
+                    # label_list += [[0] * DefaultConfig.HYPER.LABEL_DIM for count in range(self.seq_len - len(label_list))]
                 label_content = torch.LongTensor(label_list)
                 return data_content, label_content
         else:
