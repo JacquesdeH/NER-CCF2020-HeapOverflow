a3aa02533f520b87ce741601a568720c16b49629
李明昕 <18376161@buaa.edu.cn>
2020-12-04 00:05:34 +0800

    instructor v0.2

--- core/Instructor.py
+++ core/Instructor.py
@@ -5,11 +5,14 @@ import torch
 import torch.nn as nn
 import os
 from .utils import alloc_logger
+from core.model.Net import Net
+import json
+from Main import args
 
 
 class TempModule(nn.Module):
     def __init__(self):
-        super().__init__()
+        super(TempModule, self).__init__()
         self.count = 0
 
     def forward(self, words):
@@ -17,75 +20,87 @@ class TempModule(nn.Module):
         return torch.zeros((config.HYPER.BATCH_SIZE, config.HYPER.SEQ_LEN, config.HYPER.LABEL_DIM))
 
 
-def get_loss_fn(reduce=None, size_average=None):
-    return nn.CrossEntropyLoss(reduce=reduce, size_average=size_average)
+class Instructor:
+    def __init__(self, model_name, args):
+        self.model_name = model_name
+        self.args = args
+        self.model = Net(self.model_name, self.args)
+        pass
 
+    def get_loss_fn(self, reduce=None, size_average=None):
+        return nn.CrossEntropyLoss(reduce=reduce, size_average=size_average)
 
-def get_optimizer(params, lr=1e-3):
-    # torch.optim.AdamW
-    return torch.optim.Adam(params=params, lr=lr)
+    def get_optimizer(self, params, lr=1e-3):
+        # torch.optim.AdamW
+        return torch.optim.Adam(params=params, lr=lr)
 
+    def get_scheduler(self, optimizer, rate):
+        pass
 
-def get_scheduler(optimizer, rate):
-    pass
+    def save_module(self, module: nn.Module):
+        os.makedirs(config.PATHS.DATA_MODULE)
+        torch.save(module.state_dict(), config.PATHS.DATA_MODULE + "/module.th")
 
+    def load_module(self, module: nn.Module):
+        module.load_state_dict(torch.load(config.PATHS.DATA_MODULE + 'module.th'))
 
-def save_module(module: nn.Module):
-    os.makedirs(config.PATHS.DATA_MODULE)
-    torch.save(module.state_dict(), config.PATHS.DATA_MODULE + "/module.th")
+    '''
+    return batch_size and learning_rate
+    data_content: list: batch_size
+    label_content: [batch_size, seq_len] 
+    '''
 
-
-def load_module(module: nn.Module):
-    module.load_state_dict(torch.load(config.PATHS.DATA_MODULE + 'module.th'))
-
-
-'''
-return batch_size and learning_rate
-data_content: list: batch_size
-label_content: [batch_size, seq_len] 
-'''
-
-
-def train(args):
-    n_time = args.n
-    k_fold = args.k
-    train_log = alloc_logger("train.log", "train")
-    train_log.log_message("train at n_time: %d, k_fold: %d" % (n_time, k_fold))
-    dataloader = CCFDataloader(args=args, in_train=True)
-    module = TempModule()
-    loss_fn = get_loss_fn()
-    optimizer = get_optimizer(module.parameters())
-    # schedule = torch.optim.lr_scheduler.ExponentialLR(optimizer, 0.5)
-    k_fold = KFold(dataloader=dataloader, k=k_fold)
-    loss_history = list()
-    for time in range(n_time):
-        total_loss = 0.
-        for fold in range(len(k_fold)):
-            for data_content, label_content in k_fold.get_train():
-                label_predict = module(dataloader)
-                loss = loss_fn(label_predict, label_content)
-
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
-
-            for data_content, label_content in k_fold.get_valid():
-                with torch.no_grad():
-                    label_predict = module(dataloader)
+    def train(self):
+        n_time = self.args.n
+        k_fold = self.args.k
+        train_log = alloc_logger("train.log", "train")
+        train_log.log_message("train at n_time: %d, k_fold: %d" % (n_time, k_fold))
+        dataloader = CCFDataloader(args=self.args, in_train=True)
+        loss_fn = self.get_loss_fn()
+        optimizer = self.get_optimizer(self.model.parameters())
+        # schedule = torch.optim.lr_scheduler.ExponentialLR(optimizer, 0.5)
+        k_fold = KFold(dataloader=dataloader, k=k_fold)
+        loss_history = list()
+        for time in range(n_time):
+            total_loss = 0.
+            for fold in range(len(k_fold)):
+                for data_content, label_content in k_fold.get_train():
+                    label_predict = self.model(dataloader)
                     loss = loss_fn(label_predict, label_content)
-                    total_loss += loss.sum().item()
 
-            k_fold.next_fold()
-        k_fold.new_k_fold()
-        train_log.log_message('total loss: %d' % total_loss)
-        loss_history.append(total_loss)
-    return module
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+
+                for data_content, label_content in k_fold.get_valid():
+                    with torch.no_grad():
+                        label_predict = self.model(dataloader)
+                        loss = loss_fn(label_predict, label_content)
+                        total_loss += loss.sum().item()
+
+                k_fold.next_fold()
+            k_fold.new_k_fold()
+            train_log.log_message('total loss: %d' % total_loss)
+            loss_history.append(total_loss)
+
+    def test(self, args, module):
+        with torch.no_grad():
+            dataloader = CCFDataloader(args=args, in_train=False)
+            file_count = 0
+            for data_contents in dataloader:
+                predicts = module(data_contents)
+                predicts = predicts.cpu()   #[batch_size, seq_len, label_dim]
+                for predict_count in range(predicts.shape[0]):
+                    predict = predicts[predict_count]
+                    result = list()
+                    for col_num in range(predict.shape[0]):
+                        col = predict[col_num]
+                        result.append(col.argmax().item())
+                    with open(os.path.join(config.PATHS.DATA_CCF_CLEANED,'test/label/%d.json' % (file_count + predict_count)),'w') as fw:
+                        json.dump(result, fw)
+                file_count += predicts.shape[0]
 
 
-'''
-def train(k_fold=False):
-    module = TempModule()
-    dataloader = CCFDataloader()
-    loss_fn = get_loss_fn()
-    optimizer = get_optimizer(module.parameters())
-'''
+if __name__ == '__main__':
+    insructor = Instructor("a", args=args)
+    insructor.test(args=args, module=TempModule())
